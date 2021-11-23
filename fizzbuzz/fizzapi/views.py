11f52e85c2c35634f9d4c1ff6db7859a98548612
pvelarde <pvelard@bu.edu>
2021-11-23 01:19:00 -0700

    Removed unused code

--- fizzbuzz/fizzapi/views.py
+++ fizzbuzz/fizzapi/views.py
@@ -1,13 +1,9 @@
-from django.shortcuts import render
-from django.http import Http404, HttpResponse
-from rest_framework import permissions, status, viewsets
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import FizzBuzz
 from .serializers import FizzBuzzSerializer
-
-def index(request):
-    return HttpResponse("Hello, world. Youre here")
 
 class FizzBuzzView(APIView):
     """
